ff2c011c0a80ef7425c442e84e7a578dd11eb70a
Dimas <99049036+Dimas000000@users.noreply.github.com>
2023-11-24 14:24:39 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(()=>{
+    $('#header-show-btn').hide();
+
+    $('#header-hide-btn').click(()=>{
+    $('#header-show-btn').show();
+    $('header').hide();
+        $('body').css({
+            'height': '110vh',
+            'grid-template': '1fr min(5vw, 10vh) / 1fr'
+        });
+    });
+    $('#header-show-btn').click(()=>{
+    $('#header-show-btn').hide();
+    $('header').show();
+        $('body').css({
+            'height': '120vh',
+            'grid-template': 'min(5vw, 10vh) 1fr min(5vw, 10vh) / 1fr'
+        });
+    });
+});
